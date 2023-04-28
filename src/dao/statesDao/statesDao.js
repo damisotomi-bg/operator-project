df6500b7091fb4def610b5772e4ec9a352c3f131
sotomi Oluwadamilola <sotomidamilola@gmail.com>
2023-04-28 21:41:41 +0100

    Reconstructed the result returned to the client at operator registration and product-seed-type selection

--- src/dao/statesDao/statesDao.js
+++ src/dao/statesDao/statesDao.js
@@ -19,12 +19,13 @@ const getAStateLgas = async (req) => {
     try {
         const { state_id } = req.params
         const conn = await pool.connect()
-        const sql = 'SELECT * from lgas where state_id =($1);'
+        const sql = 'SELECT lgas.lga_id, lgas.lga, states.state as State FROM lgas JOIN states ON lgas.state_id = states.state_id WHERE lgas.state_id = $1;'
         const result = await conn.query(sql, [state_id])
         const rows = result.rows
         conn.release()
-
+        console.log(rows);
         return (rows)
+
 
     } catch (error) {
         throw (error)
